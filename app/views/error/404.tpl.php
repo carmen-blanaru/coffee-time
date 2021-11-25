7ea50412e4ffeec018f81c2c78d24a865d411da6
Carmen Blanaru <blanaru.pro@gmail.com>
2021-11-25 15:42:26 +0100

    creation de l'integration origin

--- app/views/error/404.tpl.php
+++ app/views/error/404.tpl.php
@@ -1,7 +1,10 @@
-<h1>404 Not Found</h1>
+<h1 class="text-white fs-1 ms-4">404 Not Found</h1>
 
-<p>
-    La ressource demandée n'existe pas...
+<p class="text-white fs-1 ms-4">
+
+    Oups! Besoin d'un café ?
 </p>
 
-<a href="<?= $router->generate('main-home') ?>">retour à l'accueil</a>
+<a href="<?= $router->generate('main-home') ?>">
+    <button class="btn btn-warning fs-5 ms-3 mb-5" type="button"> Retour à l'accueil</button>
+</a>
--- app/views/layout/footer.tpl.php
+++ app/views/layout/footer.tpl.php
@@ -1,18 +1,16 @@
 <!-- And for every user interaction, we import Bootstrap JS components -->
+</main>
 <footer>
-                <p class="text-white fs-4 px-5">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut tenetur minima magnam voluptates incidunt quos in, vero, quis illo cumque laborum dolorem ad, nisi vel unde ratione illum perferendis obcaecati.
-                </p>
-                <ul class="d-flex justify-content-center list-group list-group-horizontal mt-5 pb-3">
-                    <li class="list-group-item border-0 bg-transparent fs-4"><a href="#" class="link-warning text-decoration-none">Nous contacter</a></li>
-                    <li class="list-group-item border-0 bg-transparent fs-4"><a href="#" class="link-warning text-decoration-none">Mentions Légales</a></li>
-                    <li class="list-group-item border-0 bg-transparent fs-4"><a href="#" class="link-warning text-decoration-none">CGU</a></li>
-                </ul>
-                <p class="d-flex justify-content-center text-white fs-6 mb-5 pb-4">
-                    2021 Carmen BLANARU - Tous droits réservés! 
-                </p>
-            </footer>
-    </div>
+    <ul class="d-flex justify-content-center list-group list-group-horizontal mt-5 pb-3">
+        <li class="list-group-item border-0 bg-transparent fs-4"><a href="#" class="link-warning text-decoration-none">Nous contacter</a></li>
+        <li class="list-group-item border-0 bg-transparent fs-4"><a href="#" class="link-warning text-decoration-none">Mentions Légales</a></li>
+        <li class="list-group-item border-0 bg-transparent fs-4"><a href="#" class="link-warning text-decoration-none">CGU</a></li>
+    </ul>
+    <p class="d-flex justify-content-center text-white fs-6 mb-5 pb-4">
+        2021 Carmen BLANARU - Tous droits réservés! 
+    </p>
+</footer>
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
