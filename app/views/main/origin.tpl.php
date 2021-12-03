c84f0da2cc2d52364f071b9f5721d345e2b6dce2
Carmen Blanaru <blanaru.pro@gmail.com>
2021-12-03 14:42:20 +0100

    modification css images impaires

--- app/views/main/origin.tpl.php
+++ app/views/main/origin.tpl.php
@@ -1,5 +1,4 @@
 
-
 <h2 class="text-white text-center fs-1 mt-3 mb-5">
     D'où viennent nos cafés?
 </h2>
@@ -11,22 +10,22 @@
        
 ?>
     <img align="right"
-    src="../public/assets/img/<?= $element['url-image'] ?>" class="img-fluid rounded w-25 ms-5 me-2" alt="image carte du monde" width="800px" height="600px">
+    src="../public/assets/img/<?= $element['url-image'] ?>" class="img-fluid rounded w-25 me-3 mt-4" alt="image carte du monde" width="800px" height="600px">
         <p class="text-white fs-4 mt-5 px-5">
             <?= $element['texte'] ?>
         </p>
 
-    <?php 
-        else:
-    ?>
+<?php 
+    else:
+?>
 
-    <img align="left"
-    src="../public/assets/img/<?= $element['url-image'] ?>" class="img-fluid rounded w-25 ms-5 me-2" alt="image carte du monde" width="800px" height="600px">
-        <p class="text-white fs-4 mt-5 px-5">
-            <?= $element['texte'] ?>
-        </p>
+<img align="left"
+src="../public/assets/img/<?= $element['url-image'] ?>" class="img-fluid rounded w-25 ms-5 me-2 mt-4" alt="image carte du monde" width="800px" height="600px">
+    <p class="text-white fs-4 mt-5 px-5">
+        <?= $element['texte'] ?>
+    </p>
 
-    <?php
+<?php
     endif;
     endforeach
-    ?>
+?>
