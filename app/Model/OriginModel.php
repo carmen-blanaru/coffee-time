0a51a9309e552ecb7ef28dca88a7c11bfa53687a
Carmen Blanaru <blanaru.pro@gmail.com>
2021-12-03 14:37:12 +0100

    affichage des images en fonction des index: paire ou impaire

--- app/Model/OriginModel.php
+++ app/Model/OriginModel.php
@@ -9,26 +9,63 @@ class OriginModel
     {
         return self::$originData;
     }
+
     
-    private static $originData = 
+    public static $originData = 
     [
-        1 => [
+        0 => [
         
             'url-image' => "pexels-monstera-7411976.jpg",
             'texte' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, vel est dolores, odit ducimus similique voluptates, excepturi doloremque totam  asperiores quasi. Aspernatur aliquid et accusantium aliquam in. Quibusdam, ratione impedit!Orci varius natoque penatibus et magnis dis parturient montes, nascetur  ridiculus mus. Donec egestas eros ut orci lacinia, eget gravida enim posuere. Cras tincidunt urna sit amet purus faucibus ornare. Nam euismod elit vulputate purus   venenatis, eget cursus est feugiat. Mauris sit amet pulvinar lorem, ac porta velit. Sed a placerat urna. Nam vitae fermentum quam. Donec nec magna in lectus lacinia  sagittis. Nam fermentum hendrerit sollicitudin. Integer eu arcu sit amet nulla facilisis imperdiet."
         ],
-        2 => [
+        1 => [
             
             'texte' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, vel est dolores, odit ducimus similique voluptates, excepturi doloremque totam     asperiores quasi. Aspernatur aliquid et accusantium aliquam in. Quibusdam, ratione impedit!Orci varius natoque penatibus et magnis dis parturient montes, nascetur  ridiculus mus. Donec egestas eros ut orci lacinia, eget gravida enim posuere. Cras tincidunt urna sit amet purus faucibus ornare. Nam euismod elit vulputate purus   venenatis, eget cursus est feugiat. Mauris sit amet pulvinar lorem, ac porta velit. Sed a placerat urna. Nam vitae fermentum quam. Donec nec magna in lectus lacinia  sagittis. Nam fermentum hendrerit sollicitudin. Integer eu arcu sit amet nulla facilisis imperdiet.",
             'url-image' => "pexels-pixabay-235925.jpg", 
         ],
-        3 => [
+        2 => [
             
             'texte' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, vel est dolores, odit ducimus similique voluptates, excepturi doloremque totam     asperiores quasi. Aspernatur aliquid et accusantium aliquam in. Quibusdam, ratione impedit!Orci varius natoque penatibus et magnis dis parturient montes, nascetur  ridiculus mus. Donec egestas eros ut orci lacinia, eget gravida enim posuere. Cras tincidunt urna sit amet purus faucibus ornare. Nam euismod elit vulputate purus   venenatis, eget cursus est feugiat. Mauris sit amet pulvinar lorem, ac porta velit. Sed a placerat urna. Nam vitae fermentum quam. Donec nec magna in lectus lacinia  sagittis. Nam fermentum hendrerit sollicitudin. Integer eu arcu sit amet nulla facilisis imperdiet.",
             'url-image' => "pexels-michael-burrows-7125540.jpg", 
         ],
 
     ];
+
+    /*public static function originData()
+    {
+        $originData = 
+    [
+        0 => [
+        
+            'url-image' => "pexels-monstera-7411976.jpg",
+            'texte' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, vel est dolores, odit ducimus similique voluptates, excepturi doloremque totam  asperiores quasi. Aspernatur aliquid et accusantium aliquam in. Quibusdam, ratione impedit!Orci varius natoque penatibus et magnis dis parturient montes, nascetur  ridiculus mus. Donec egestas eros ut orci lacinia, eget gravida enim posuere. Cras tincidunt urna sit amet purus faucibus ornare. Nam euismod elit vulputate purus   venenatis, eget cursus est feugiat. Mauris sit amet pulvinar lorem, ac porta velit. Sed a placerat urna. Nam vitae fermentum quam. Donec nec magna in lectus lacinia  sagittis. Nam fermentum hendrerit sollicitudin. Integer eu arcu sit amet nulla facilisis imperdiet."
+        ],
+        1 => [
+            
+            'texte' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, vel est dolores, odit ducimus similique voluptates, excepturi doloremque totam     asperiores quasi. Aspernatur aliquid et accusantium aliquam in. Quibusdam, ratione impedit!Orci varius natoque penatibus et magnis dis parturient montes, nascetur  ridiculus mus. Donec egestas eros ut orci lacinia, eget gravida enim posuere. Cras tincidunt urna sit amet purus faucibus ornare. Nam euismod elit vulputate purus   venenatis, eget cursus est feugiat. Mauris sit amet pulvinar lorem, ac porta velit. Sed a placerat urna. Nam vitae fermentum quam. Donec nec magna in lectus lacinia  sagittis. Nam fermentum hendrerit sollicitudin. Integer eu arcu sit amet nulla facilisis imperdiet.",
+            'url-image' => "pexels-pixabay-235925.jpg", 
+        ],
+        2 => [
+            
+            'texte' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, vel est dolores, odit ducimus similique voluptates, excepturi doloremque totam     asperiores quasi. Aspernatur aliquid et accusantium aliquam in. Quibusdam, ratione impedit!Orci varius natoque penatibus et magnis dis parturient montes, nascetur  ridiculus mus. Donec egestas eros ut orci lacinia, eget gravida enim posuere. Cras tincidunt urna sit amet purus faucibus ornare. Nam euismod elit vulputate purus   venenatis, eget cursus est feugiat. Mauris sit amet pulvinar lorem, ac porta velit. Sed a placerat urna. Nam vitae fermentum quam. Donec nec magna in lectus lacinia  sagittis. Nam fermentum hendrerit sollicitudin. Integer eu arcu sit amet nulla facilisis imperdiet.",
+            'url-image' => "pexels-michael-burrows-7125540.jpg", 
+        ],
+
+    ];
+    $odd = [];
+    $even = [];
+
+    foreach ($originData as $key => $value) {
+        if( 0 == $key%2) {
+            $even[] = $value;
+        } else {
+            $odd[] = $value;
+        }
+    }
+        return $value;
+    }*/
+
+     
     
 }
 
--- app/views/main/origin.tpl.php
+++ app/views/main/origin.tpl.php
@@ -7,26 +7,26 @@
 <?php 
 
     foreach ($origin as $index => $element):
-
+       if($index&1):
+       
 ?>
+    <img align="right"
+    src="../public/assets/img/<?= $element['url-image'] ?>" class="img-fluid rounded w-25 ms-5 me-2" alt="image carte du monde" width="800px" height="600px">
+        <p class="text-white fs-4 mt-5 px-5">
+            <?= $element['texte'] ?>
+        </p>
+
+    <?php 
+        else:
+    ?>
+
     <img align="left"
     src="../public/assets/img/<?= $element['url-image'] ?>" class="img-fluid rounded w-25 ms-5 me-2" alt="image carte du monde" width="800px" height="600px">
         <p class="text-white fs-4 mt-5 px-5">
             <?= $element['texte'] ?>
         </p>
 
-   <!-- <img align="right"
-    src="../public/assets/img/pexels-pixabay-235925.jpg" class="img-fluid rounded w-25 me-3 mt-4" alt="image plantation de café" width="800px" height="600px">
-        <p class="text-white fs-4 mt-5 px-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, vel est dolores, odit ducimus similique voluptates, excepturi doloremque totam asperiores quasi. Aspernatur aliquid et accusantium aliquam in. Quibusdam, ratione impedit!Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec egestas eros ut orci lacinia, eget gravida enim posuere. Cras tincidunt urna sit amet purus faucibus ornare. Nam euismod elit vulputate purus venenatis, eget cursus est feugiat. Mauris sit amet pulvinar lorem, ac porta velit. Sed a placerat urna. Nam vitae fermentum quam. Donec nec magna in lectus lacinia sagittis. Nam fermentum hendrerit sollicitudin. Integer eu arcu sit amet nulla facilisis imperdiet. 
-        </p>
-
-    <img align="left"
-    src="../public/assets/img/pexels-michael-burrows-7125540.jpg" class="img-fluid rounded w-25 ms-5 me-2 mt-4" alt="image personnes cueillant du café" width="800px" height="600px">
-        <p class="text-white fs-4 mt-5 px-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, vel est dolores, odit ducimus similique voluptates, excepturi doloremque totam asperiores quasi. Aspernatur aliquid et accusantium aliquam in. Quibusdam, ratione impedit!Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec egestas eros ut orci lacinia, eget gravida enim posuere. Cras tincidunt urna sit amet purus faucibus ornare. Nam euismod elit vulputate purus venenatis, eget cursus est feugiat. Mauris sit amet pulvinar lorem, ac porta velit. Sed a placerat urna. Nam vitae fermentum quam. Donec nec magna in lectus lacinia sagittis. Nam fermentum hendrerit sollicitudin. Integer eu arcu sit amet nulla facilisis imperdiet. 
-        </p>-->
-
     <?php
+    endif;
     endforeach
     ?>
