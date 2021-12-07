c4e90deeea6ee2e222881d259da79ad26b5ddab0
Carmen Blanaru <blanaru.pro@gmail.com>
2021-12-07 18:29:05 +0100

    création de la page des cafés

--- app/views/main/coffee.tpl.php
+++ app/views/main/coffee.tpl.php
@@ -1,3 +1,3 @@
-<h2 class="text-white text-center fs-1 mt-3 mb-5"> Nos choix de cafés</h2>
-    <img src="<?=$assetsBaseUri ?>img/">
+<h2 class="text-white text-center fs-1 mt-5 mb-3"> Nos choix de cafés</h2>
+    <img src="<?=$assetsBaseUri ?>img/coffee-g152bdc3b3_1280.png" class="img-fluid position-relative top-50 start-50 translate-middle-x" width="1000px" height="900px" alt="types de cafés" >
     
