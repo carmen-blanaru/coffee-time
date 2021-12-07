5fa1d3dff06782868a871ab15b3d72d3ab530959
Carmen Blanaru <blanaru.pro@gmail.com>
2021-12-07 17:48:05 +0100

    ajout des dernières photos

--- app/views/main/coffee.tpl.php
+++ app/views/main/coffee.tpl.php
@@ -1,23 +1,3 @@
 <h2 class="text-white text-center fs-1 mt-3 mb-5"> Nos choix de cafés</h2>
-    <div class="d-sm-flex ms-4">
-        <div class="card bg-warning ms-5 mb-2" style="width: 10rem;">
-            <img src="../public/assets/img/espresso.jpeg" class="card-img-top" alt="image café espresso">
-            <div class="card-body pt-0" style="height: 3rem;">
-                <p class="card-text-center fs-4 fw-bold ps-4 pt-2"> Espresso </p>
-            </div>
-        </div>
-
-        <div class="card bg-warning ms-5 mb-2" style="width: 10rem;">
-            <img src="../public/assets/img/ristreto.jpeg" class="card-img-top" style="height: 9rem;" alt="image café ristreto">
-            <div class="card-body pt-0" style="height: 3rem;">
-                <p class="card-text-center fs-4 fw-bold ps-4 pt-2 mb-2"> Ristreto </p>
-            </div>
-        </div>
-
-        <div class="card bg-warning ms-5" style="width: 10rem;">
-            <img src="../public/assets/img/americano.jpeg" class="card-img-top" style="height: 9rem;" alt="image café americano">
-            <div class="card-body pt-0" style="height: 2em;">
-                <p class="card-text-center fs-4 fw-bold ps-4 pt-2"> Americano </p>
-            </div>
-        </div>
-    </div>
+    <img src="<?=$assetsBaseUri ?>img/">
+    
